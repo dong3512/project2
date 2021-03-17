0448e0fee19e22d8d6548b3c2c7adfaf218b88fd
dong3512 <dlehddls1015@naver.com>
2021-03-17 20:51:46 +0900

    .

--- project-server/app/src/main/java/com/dong/pms/table/SeatTable.java
+++ project-server/app/src/main/java/com/dong/pms/table/SeatTable.java
@@ -25,6 +25,7 @@ public class SeatTable implements DataTable {
       case "seat/insert":
 
         fields = request.getData().get(0).split(",");
+
         seat = new Seat();
 
         if (list.size() > 0) {
@@ -42,7 +43,68 @@ public class SeatTable implements DataTable {
 
         JsonFileHandler.saveObjects(jsonFile, list);
         break;
+      case "seat/selectall":
+        for (Seat t : list) {
+          response.appendData(String.format("%d,%s,%d,%s,%s", 
+              t.getNo(),
+              t.getMgrade(),
+              t.getSgrade(),
+              t.getSno(),
+              t.getEtc()));
+        } 
+        break;
+      case "seat/select":
+        int no = Integer.parseInt(request.getData().get(0));
 
+        seat = getSeat(no);
+        if (seat != null) {
+          response.appendData(String.format("%d,%s,%d,%s,%s",
+              seat.getNo(),
+              seat.getMgrade(),
+              seat.getSgrade(),
+              seat.getSno(),
+              seat.getEtc()));
+        } else {
+          throw new Exception("해당 번호의 좌석이 없습니다.");
+        }
+        break;
+      case "seat/update":
+        fields = request.getData().get(0).split(",");
+
+        seat = getSeat(Integer.parseInt(fields[0]));
+        if (seat == null) {
+          throw new Exception("해당 번호의 좌석이 없습니다.");
+        }
+
+        seat.setMgrade(fields[1]);
+        seat.setSgrade(Integer.parseInt(fields[2]));
+        seat.setSno(fields[3]);
+        seat.setEtc(fields[4]);
+
+        JsonFileHandler.saveObjects(jsonFile, list);
+        break;
+      case "seat/delete":
+        no = Integer.parseInt(request.getData().get(0));
+        seat = getSeat(no);
+        if (seat == null) {
+          throw new Exception("해당 번호의 좌석이 없습니다.");
+        }
+
+        list.remove(seat);
+
+        JsonFileHandler.saveObjects(jsonFile, list);
+        break;
+      default:
+        throw new Exception("해당 명령을 처리 할 수 없습니다.");
     }
+  }
+
+  private Seat getSeat(int seatNo) {
+    for (Seat t : list) {
+      if (t.getNo() == seatNo) {
+        return t;
+      }
+    }
+    return null;
   }
 }
